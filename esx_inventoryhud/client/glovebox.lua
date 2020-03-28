ea5878b4cac25fd066661727d40f67e42e2afc54
rex2630 <rex2630@gmail.com>
2020-03-28 01:31:02 +0100

    Update glovebox.lua

--- esx_inventoryhud/client/glovebox.lua
+++ esx_inventoryhud/client/glovebox.lua
@@ -1,32 +1,21 @@
-local gloveboxData = nil
+local gloveboxData
 
 RegisterNetEvent("esx_inventoryhud:openGloveboxInventory")
-AddEventHandler(
-    "esx_inventoryhud:openGloveboxInventory",
-    function(data, blackMoney, inventory, weapons)
-        setGloveboxInventoryData(data, blackMoney, inventory, weapons)
-        TriggerScreenblurFadeIn(0)
-        openGloveboxInventory()
-    end
-)
+AddEventHandler("esx_inventoryhud:openGloveboxInventory", function(data, blackMoney, inventory, weapons)
+	setGloveboxInventoryData(data, blackMoney, inventory, weapons)
+	TriggerScreenblurFadeIn(0)
+	openGloveboxInventory()
+end)
 
 RegisterNetEvent("esx_inventoryhud:refreshGloveboxInventory")
-AddEventHandler(
-    "esx_inventoryhud:refreshGloveboxInventory",
-    function(data, blackMoney, inventory, weapons)
-        setGloveboxInventoryData(data, blackMoney, inventory, weapons)
-    end
-)
+AddEventHandler("esx_inventoryhud:refreshGloveboxInventory", function(data, blackMoney, inventory, weapons)
+	setGloveboxInventoryData(data, blackMoney, inventory, weapons)
+end)
 
 function setGloveboxInventoryData(data, blackMoney, inventory, weapons)
     gloveboxData = data
 
-    SendNUIMessage(
-        {
-            action = "setInfoText",
-            text = data.text
-        }
-    )
+    SendNUIMessage({action = "setInfoText",text = data.text})
 
     items = {}
 
@@ -80,58 +69,42 @@ function setGloveboxInventoryData(data, blackMoney, inventory, weapons)
         end
     end
 
-    SendNUIMessage(
-        {
-            action = "setSecondInventoryItems",
-            itemList = items
-        }
-    )
+    SendNUIMessage({action = "setSecondInventoryItems", itemList = items})
 end
 
 function openGloveboxInventory()
     loadPlayerInventory()
     isInInventory = true
 
-    SendNUIMessage(
-        {
-            action = "display",
-            type = "glovebox"
-        }
-    )
+    SendNUIMessage({action = "display", type = "glovebox"})
 
     SetNuiFocus(true, true)
 end
 
-RegisterNUICallback(
-    "PutIntoGlovebox",
-    function(data, cb)
-        if type(data.number) == "number" and math.floor(data.number) == data.number then
-            local count = tonumber(data.number)
+RegisterNUICallback("PutIntoGlovebox", function(data, cb)
+	if type(data.number) == "number" and math.floor(data.number) == data.number then
+		local count = tonumber(data.number)
 
-            if data.item.type == "item_weapon" then
-                count = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(data.item.name))
-            end
+	if data.item.type == "item_weapon" then
+		count = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(data.item.name))
+	end
 
-            TriggerServerEvent("esx_glovebox:putItem", gloveboxData.plate, data.item.type, data.item.name, count, gloveboxData.max, gloveboxData.myVeh, data.item.label)
-        end
+		TriggerServerEvent("esx_glovebox:putItem", gloveboxData.plate, data.item.type, data.item.name, count, gloveboxData.max, gloveboxData.myVeh, data.item.label)
+	end
 
         Wait(500)
         loadPlayerInventory()
 
         cb("ok")
-    end
-)
+end)
 
-RegisterNUICallback(
-    "TakeFromGlovebox",
-    function(data, cb)
-        if type(data.number) == "number" and math.floor(data.number) == data.number then
-            TriggerServerEvent("esx_glovebox:getItem", gloveboxData.plate, data.item.type, data.item.name, tonumber(data.number), gloveboxData.max, gloveboxData.myVeh)
-        end
+RegisterNUICallback("TakeFromGlovebox", function(data, cb)
+	if type(data.number) == "number" and math.floor(data.number) == data.number then
+		TriggerServerEvent("esx_glovebox:getItem", gloveboxData.plate, data.item.type, data.item.name, tonumber(data.number), gloveboxData.max, gloveboxData.myVeh)
+	end
 
-        Wait(500)
+		Wait(500)
         loadPlayerInventory()
 
         cb("ok")
-    end
-)
+end)
