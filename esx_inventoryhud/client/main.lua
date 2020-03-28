dc9b82efc582573663de1f208728cfebec543603
rex2630 <rex2630@gmail.com>
2020-03-28 01:30:34 +0100

    Update main.lua

--- esx_inventoryhud/client/main.lua
+++ esx_inventoryhud/client/main.lua
@@ -1,16 +1,4 @@
-local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-  }
-
-local trunkData = nil
+local trunkData
 local isInInventory = false
 ESX = nil
 local fastWeapons = {
@@ -22,16 +10,10 @@ local fastWeapons = {
 Citizen.CreateThread(
     function()
         while ESX == nil do
-            TriggerEvent(
-                "esx:getSharedObject",
-                function(obj)
-                    ESX = obj
-                end
-            )
+            TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
             Citizen.Wait(0)
         end
-    end
-)
+end)
 
 Citizen.CreateThread(
     function()
@@ -43,19 +25,13 @@ Citizen.CreateThread(
                 openInventory()
             end
         end
-    end
-)
+    end)
 
 function openInventory()
     loadPlayerInventory()
     isInInventory = true
     
-    SendNUIMessage(
-        {
-            action = "display",
-            type = "normal"
-        }
-    )
+    SendNUIMessage({action = "display", type = "normal"})
     SetNuiFocus(true, true)
 end
 
@@ -63,61 +39,47 @@ function openTrunkInventory()
     loadPlayerInventory()
     isInInventory = true
 
-    SendNUIMessage(
-        {
-            action = "display",
-            type = "trunk"
-        }
-    )
+    SendNUIMessage({action = "display", type = "trunk"})
 
     SetNuiFocus(true, true)
 end
 
 function closeInventory()
     isInInventory = false
-    SendNUIMessage(
-        {
-            action = "hide"
-        }
-    )
+    SendNUIMessage({action = "hide"})
     SetNuiFocus(false, false)
     TriggerScreenblurFadeOut(0)
     ClearPedSecondaryTask(GetPlayerPed(-1))
 end
 
-RegisterNUICallback(
-    "NUIFocusOff",
-    function()
-        closeInventory()
-        Citizen.Wait(50)
-        TriggerScreenblurFadeOut(0)
-    end
-)
+RegisterNUICallback("NUIFocusOff", function()
+	closeInventory()
+	Citizen.Wait(50)
+	TriggerScreenblurFadeOut(0)
+end)
 
-RegisterNUICallback(
-    "GetNearPlayers",
-    function(data, cb)
-        local playerPed = PlayerPedId()
-        local players, nearbyPlayer = ESX.Game.GetPlayersInArea(GetEntityCoords(playerPed), 3.0)
-        local foundPlayers = false
-        local elements = {}
+RegisterNUICallback("GetNearPlayers", function(data, cb)
+	local playerPed = PlayerPedId()
+	local players, nearbyPlayer = ESX.Game.GetPlayersInArea(GetEntityCoords(playerPed), 3.0)
+	local foundPlayers = false
+	local elements = {}
 
-        for i = 1, #players, 1 do
-            if players[i] ~= PlayerId() then
-                foundPlayers = true
+	for i = 1, #players, 1 do
+		if players[i] ~= PlayerId() then
+			foundPlayers = true
 
-                table.insert(
-                    elements,
+			table.insert(
+				elements,
                     {
                         label = GetPlayerName(players[i]),
                         player = GetPlayerServerId(players[i])
                     }
                 )
-            end
         end
+    end
 
-        if not foundPlayers then
-            --[[exports.pNotify:SendNotification(
+	if not foundPlayers then
+		--[[exports.pNotify:SendNotification(
                 {
                     text = _U("players_nearby"),
                     type = "error",
@@ -139,100 +101,85 @@ RegisterNUICallback(
         end
 
         cb("ok")
-    end
-)
+    end)
 
-RegisterNUICallback(
-    "PutIntoTrunk",
-    function(data, cb)
-        if IsPedSittingInAnyVehicle(playerPed) then
-            return
-        end
+RegisterNUICallback("PutIntoTrunk", function(data, cb)
+	if IsPedSittingInAnyVehicle(playerPed) then
+		return
+	end
 
-        if type(data.number) == "number" and math.floor(data.number) == data.number then
-            local count = tonumber(data.number)
+	if type(data.number) == "number" and math.floor(data.number) == data.number then
+		local count = tonumber(data.number)
 
-            if data.item.type == "item_weapon" then
-                count = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(data.item.name))
-            end
+	if data.item.type == "item_weapon" then
+		count = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(data.item.name))
+	end
 
-            TriggerServerEvent("esx_trunk:putItem", trunkData.plate, data.item.type, data.item.name, count, trunkData.max, trunkData.myVeh, data.item.label)
-        end
+		TriggerServerEvent("esx_trunk:putItem", trunkData.plate, data.item.type, data.item.name, count, trunkData.max, trunkData.myVeh, data.item.label)
+	end
 
         Wait(500)
         loadPlayerInventory()
 
         cb("ok")
-    end
-)
+end)
 
-RegisterNUICallback(
-    "TakeFromTrunk",
-    function(data, cb)
-        if IsPedSittingInAnyVehicle(playerPed) then
-            return
-        end
+RegisterNUICallback("TakeFromTrunk", function(data, cb)
+	if IsPedSittingInAnyVehicle(playerPed) then
+		return
+	end
 
-        if type(data.number) == "number" and math.floor(data.number) == data.number then
-            TriggerServerEvent("esx_trunk:getItem", trunkData.plate, data.item.type, data.item.name, tonumber(data.number), trunkData.max, trunkData.myVeh)
-        end
+	if type(data.number) == "number" and math.floor(data.number) == data.number then
+		TriggerServerEvent("esx_trunk:getItem", trunkData.plate, data.item.type, data.item.name, tonumber(data.number), trunkData.max, trunkData.myVeh)
+	end
 
         Wait(500)
         loadPlayerInventory()
 
         cb("ok")
-    end
-)
+    end)
 
-RegisterNUICallback(
-    "UseItem",
-    function(data, cb)
+RegisterNUICallback("UseItem", function(data, cb)
         TriggerServerEvent("esx:useItem", data.item.name)
         Citizen.Wait(500)
         loadPlayerInventory()
 
         cb("ok")
-    end
-)
+    end)
 
-RegisterNUICallback(
-    "DropItem",
-    function(data, cb)
-        if IsPedSittingInAnyVehicle(playerPed) then
-            return
-        end
+RegisterNUICallback("DropItem", function(data, cb)
+	if IsPedSittingInAnyVehicle(playerPed) then
+		return
+	end
 
-        if type(data.number) == "number" and math.floor(data.number) == data.number then
-            TriggerServerEvent("esx:removeInventoryItem", data.item.type, data.item.name, data.number)
-        end
+	if type(data.number) == "number" and math.floor(data.number) == data.number then
+		TriggerServerEvent("esx:removeInventoryItem", data.item.type, data.item.name, data.number)
+	end
 
         Wait(500)
         loadPlayerInventory()
 
         cb("ok")
-    end
-)
+    end)
 
-RegisterNUICallback(
-    "GiveItem",
-    function(data, cb)
-        local playerPed = PlayerPedId()
-        local players, nearbyPlayer = ESX.Game.GetPlayersInArea(GetEntityCoords(playerPed), 3.0)
-        local foundPlayer = false
-        for i = 1, #players, 1 do
-            if players[i] ~= PlayerId() then
-                if GetPlayerServerId(players[i]) == data.player then
-                    foundPlayer = true
-                end
+RegisterNUICallback("GiveItem", function(data, cb)
+	local playerPed = PlayerPedId()
+	local players, nearbyPlayer = ESX.Game.GetPlayersInArea(GetEntityCoords(playerPed), 3.0)
+	local foundPlayer = false
+	for i = 1, #players, 1 do
+		if players[i] ~= PlayerId() then
+			if GetPlayerServerId(players[i]) == data.player then
+				foundPlayer = true
             end
         end
+    end
 
-        if foundPlayer then
-            local count = tonumber(data.number)
+	if foundPlayer then
+		local count = tonumber(data.number)
 
-            if data.item.type == "item_weapon" then
-                count = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(data.item.name))
-            end
+		if data.item.type == "item_weapon" then
+			count = GetAmmoInPedWeapon(PlayerPedId(), GetHashKey(data.item.name))
+		end
 
             TriggerServerEvent("esx:giveInventoryItem", data.player, data.item.type, data.item.name, count)
             Wait(500)
@@ -248,10 +195,9 @@ RegisterNUICallback(
                     queue = "inventoryhud"
                 }
             )--]] -- Uncomment This to Use pNotify
-        end
+	end
         cb("ok")
-    end
-)
+end)
 
 function shouldCloseInventory(itemName)
     for index, value in ipairs(Config.CloseUiItems) do
@@ -292,9 +238,7 @@ function getInventoryWeight(inventory)
 end
 
 function loadPlayerInventory()
-    ESX.TriggerServerCallback(
-        "esx_inventoryhud:getPlayerInventory",
-        function(data)
+    ESX.TriggerServerCallback("esx_inventoryhud:getPlayerInventory", function(data)
             items = {}
             fastItems = {}
             inventory = data.inventory
@@ -443,10 +387,8 @@ function loadPlayerInventory()
                     fastItems = fastItems,
 					text = texts
                 }
-            )
-			
-        end,
-        GetPlayerServerId(PlayerId())
+            )	
+        end, GetPlayerServerId(PlayerId())
     )
 end
 
@@ -459,31 +401,20 @@ function setNotHurt()
 end
 
 RegisterNetEvent("esx_inventoryhud:openTrunkInventory")
-AddEventHandler(
-    "esx_inventoryhud:openTrunkInventory",
-    function(data, blackMoney, inventory, weapons)
-        setTrunkInventoryData(data, blackMoney, inventory, weapons)
-        openTrunkInventory()
-    end
-)
+AddEventHandler("esx_inventoryhud:openTrunkInventory", function(data, blackMoney, inventory, weapons)
+	setTrunkInventoryData(data, blackMoney, inventory, weapons)
+	openTrunkInventory()
+end)
 
 RegisterNetEvent("esx_inventoryhud:refreshTrunkInventory")
-AddEventHandler(
-    "esx_inventoryhud:refreshTrunkInventory",
-    function(data, blackMoney, inventory, weapons)
-        setTrunkInventoryData(data, blackMoney, inventory, weapons)
-    end
-)
+AddEventHandler("esx_inventoryhud:refreshTrunkInventory", function(data, blackMoney, inventory, weapons)
+	setTrunkInventoryData(data, blackMoney, inventory, weapons)
+end)
 
 function setTrunkInventoryData(data, blackMoney, inventory, weapons)
     trunkData = data
 	
-		SendNUIMessage(
-			{
-				action = "setInfoText",
-				text = data.text
-			}
-		)			
+	SendNUIMessage({action = "setInfoText", text = data.text})			
 
     items = {}
 
@@ -538,12 +469,7 @@ function setTrunkInventoryData(data, blackMoney, inventory, weapons)
         end
     end
 	
-    SendNUIMessage(
-        {
-            action = "setSecondInventoryItems",
-            itemList = items
-        }
-    )
+    SendNUIMessage({action = "setSecondInventoryItems",itemList = items})
 end
 
 function openTrunkInventory()
@@ -557,12 +483,7 @@ function openTrunkInventory()
         end)
     end
 
-    SendNUIMessage(
-        {
-            action = "display",
-            type = "trunk"
-        }
-    )
+    SendNUIMessage({action = "display", type = "trunk"})
 
     SetNuiFocus(true, true)
 end
@@ -579,32 +500,32 @@ Citizen.CreateThread(
                 DisableControlAction(0, 257, true) -- Attack 2
                 DisableControlAction(0, 25, true) -- Aim
                 DisableControlAction(0, 263, true) -- Melee Attack 1
-                DisableControlAction(0, Keys["W"], true) -- W
-                DisableControlAction(0, Keys["A"], true) -- A
+                DisableControlAction(0, 32, true) -- W
+                DisableControlAction(0, 34, true) -- A
                 DisableControlAction(0, 31, true) -- S (fault in Keys table!)
                 DisableControlAction(0, 30, true) -- D (fault in Keys table!)
 
-                DisableControlAction(0, Keys["R"], true) -- Reload
-                DisableControlAction(0, Keys["SPACE"], true) -- Jump
-                DisableControlAction(0, Keys["Q"], true) -- Cover
-                DisableControlAction(0, Keys["TAB"], true) -- Select Weapon
-                DisableControlAction(0, Keys["F"], true) -- Also 'enter'?
+                DisableControlAction(0, 45, true) -- Reload
+                DisableControlAction(0, 22, true) -- Jump
+                DisableControlAction(0, 44, true) -- Cover
+                DisableControlAction(0, 37, true) -- Select Weapon
+                DisableControlAction(0, 23, true) -- Also 'enter'?
 
-                DisableControlAction(0, Keys["F1"], true) -- Disable phone
-                DisableControlAction(0, Keys["F2"], true) -- Inventory
-                DisableControlAction(0, Keys["F3"], true) -- Animations
-                DisableControlAction(0, Keys["F6"], true) -- Job
+                DisableControlAction(0, 288, true) -- Disable phone
+                DisableControlAction(0, 289, true) -- Inventory
+                DisableControlAction(0, 170, true) -- Animations
+                DisableControlAction(0, 167, true) -- Job
 
-                DisableControlAction(0, Keys["V"], true) -- Disable changing view
-                DisableControlAction(0, Keys["C"], true) -- Disable looking behind
-                DisableControlAction(0, Keys["X"], true) -- Disable clearing animation
-                DisableControlAction(2, Keys["P"], true) -- Disable pause screen
+                DisableControlAction(0, 0, true) -- Disable changing view
+                DisableControlAction(0, 26, true) -- Disable looking behind
+                DisableControlAction(0, 73, true) -- Disable clearing animation
+                DisableControlAction(2, 199, true) -- Disable pause screen
 
                 DisableControlAction(0, 59, true) -- Disable steering in vehicle
                 DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
                 DisableControlAction(0, 72, true) -- Disable reversing in vehicle
 
-                DisableControlAction(2, Keys["LEFTCTRL"], true) -- Disable going stealth
+                DisableControlAction(2, 36, true) -- Disable going stealth
 
                 DisableControlAction(0, 47, true) -- Disable weapon
                 DisableControlAction(0, 264, true) -- Disable melee
@@ -617,8 +538,7 @@ Citizen.CreateThread(
                 DisableControlAction(27, 75, true) -- Disable exit vehicle
             end
         end
-    end
-)
+    end)
 
 -- HIDE WEAPON WHEEL
 Citizen.CreateThread(function ()
@@ -628,36 +548,33 @@ Citizen.CreateThread(function ()
 		HideHudComponentThisFrame(19)
 		HideHudComponentThisFrame(20)
 		BlockWeaponWheelThisFrame()
-		DisableControlAction(0,Keys["TAB"],true)
+		DisableControlAction(0, 37,true)
 	end
 end)
+
 --FAST ITEMS
-RegisterNUICallback(
-    "PutIntoFast",
-    function(data, cb)
-		if data.item.slot ~= nil then
-			fastWeapons[data.item.slot] = nil
-		end
+RegisterNUICallback("PutIntoFast", function(data, cb)
+	if data.item.slot ~= nil then
+		fastWeapons[data.item.slot] = nil
+	end
 		fastWeapons[data.slot] = data.item.name
 		TriggerServerEvent("esx_inventoryhud:changeFastItem",data.slot,data.item.name)
 		loadPlayerInventory()
 		cb("ok")
-	end
-)
-RegisterNUICallback(
-    "TakeFromFast",
-    function(data, cb)
-		fastWeapons[data.item.slot] = nil
-		TriggerServerEvent("esx_inventoryhud:changeFastItem",0,data.item.name)
-		loadPlayerInventory()
-		cb("ok")
-	end
-)
+end)
+
+RegisterNUICallback("TakeFromFast", function(data, cb)
+	fastWeapons[data.item.slot] = nil
+	TriggerServerEvent("esx_inventoryhud:changeFastItem",0,data.item.name)
+	loadPlayerInventory()
+	cb("ok")
+end)
+
 Citizen.CreateThread(
     function()
 		while true do
             Citizen.Wait(0)
-            if IsDisabledControlJustReleased(1, Keys["1"]) then
+            if IsDisabledControlJustReleased(1, 157) then
                 if fastWeapons[1] ~= nil then
 					if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey(fastWeapons[1]) then
 						SetCurrentPedWeapon(GetPlayerPed(-1), "WEAPON_UNARMED",true)
@@ -666,7 +583,7 @@ Citizen.CreateThread(
 					end
 				end
             end
-			if IsDisabledControlJustReleased(1, Keys["2"]) then
+			if IsDisabledControlJustReleased(1, 158) then
                 if fastWeapons[2] ~= nil then
 					if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey(fastWeapons[2]) then
 						SetCurrentPedWeapon(GetPlayerPed(-1), "WEAPON_UNARMED",true)
@@ -675,7 +592,7 @@ Citizen.CreateThread(
 					end
 				end
             end
-			if IsDisabledControlJustReleased(1, Keys["3"]) then
+			if IsDisabledControlJustReleased(1, 160) then
                 if fastWeapons[3] ~= nil then
 					if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey(fastWeapons[3]) then
 						SetCurrentPedWeapon(GetPlayerPed(-1), "WEAPON_UNARMED",true)
@@ -684,7 +601,7 @@ Citizen.CreateThread(
 					end
 				end
             end
-            if IsDisabledControlJustReleased(1, Keys["4"]) then
+            if IsDisabledControlJustReleased(1, 164) then
                 if fastWeapons[4] ~= nil then
 					if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey(fastWeapons[4]) then
 						SetCurrentPedWeapon(GetPlayerPed(-1), "WEAPON_UNARMED",true)
@@ -693,7 +610,7 @@ Citizen.CreateThread(
 					end
 				end
             end
-            if IsDisabledControlJustReleased(1, Keys["5"]) then
+            if IsDisabledControlJustReleased(1, 166) then
                 if fastWeapons[5] ~= nil then
 					if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey(fastWeapons[5]) then
 						SetCurrentPedWeapon(GetPlayerPed(-1), "WEAPON_UNARMED",true)
@@ -703,12 +620,9 @@ Citizen.CreateThread(
 				end
             end
         end
-    end
-)
-
+end)
 
 --Add Items--
-
 RegisterNetEvent('esx_inventoryhud:client:addItem')
 AddEventHandler('esx_inventoryhud:client:addItem', function(itemname, itemlabel)
     local data = { name = itemname, label = itemlabel }
