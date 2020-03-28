87ad864c7724f2facc583ca5c3d85cbfef1a08d3
rex2630 <rex2630@gmail.com>
2020-03-28 01:34:48 +0100

    Update fxmanifest.lua

--- esx_inventoryhud/fxmanifest.lua
+++ esx_inventoryhud/fxmanifest.lua
@@ -1,60 +1,60 @@
 fx_version 'adamant'
-games { 'gta5' }
+game 'gta5'
 
-description "MaoriRP Inventory"
+description 'MaoriRP Inventory'
 
-version "1.0"
+version '1.0'
 
-ui_page "html/ui.html"
+ui_page 'html/ui.html'
 
 client_scripts {
-  "@es_extended/locale.lua",
-  "client/main.lua",
-  "client/property.lua",
-  "client/shop.lua",
-  "client/glovebox.lua",
-  "locales/en.lua",
-  "locales/cs.lua",
-  "locales/fr.lua",
-  "locales/pt.lua",
-  "client/disc-property.lua",
-  "client/player.lua",
-  "client/steal.lua",
-  "config.lua"
+  '@es_extended/locale.lua',
+  'client/main.lua',
+  'client/property.lua',
+  'client/shop.lua',
+  'client/glovebox.lua',
+  'locales/en.lua',
+  'locales/cs.lua',
+  'locales/fr.lua',
+  'locales/pt.lua',
+  'client/disc-property.lua',
+  'client/player.lua',
+  'client/steal.lua',
+  'config.lua'
 }
 
 server_scripts {
-  "@async/async.lua",
-  "@mysql-async/lib/MySQL.lua",
-  "@es_extended/locale.lua",
-  "server/main.lua",
-  "server/steal.lua",
-  "locales/en.lua",
-  "locales/cs.lua",
-  "locales/fr.lua",
-  "locales/pt.lua",
-  "config.lua"
+  '@async/async.lua',
+  '@mysql-async/lib/MySQL.lua',
+  '@es_extended/locale.lua',
+  'server/main.lua',
+  'server/steal.lua',
+  'locales/en.lua',
+  'locales/cs.lua',
+  'locales/fr.lua',
+  'locales/pt.lua',
+  'config.lua'
 }
 
 files {
-  "html/ui.html",
-  "html/css/ui.css",
-  "html/css/jquery-ui.css",
-  "html/js/inventory.js",
-  "html/js/config.js",
+  'html/ui.html',
+  'html/css/ui.css',
+  'html/css/jquery-ui.css',
+  'html/js/inventory.js',
+  'html/js/config.js',
   -- JS LOCALES
-  "html/locales/cs.js",
-  "html/locales/en.js",
-  "html/locales/fr.js",
+  'html/locales/cs.js',
+  'html/locales/en.js',
+  'html/locales/fr.js',
   -- IMAGES
-  "html/img/bullet.png",
-  "html/img/logo.png",
+  'html/img/bullet.png',
+  'html/img/logo.png',
   -- ICONS
   'html/img/items/*.png',
   'html/img/*.png'
 }
 
 dependencies {
-	"es_extended",
-	"mythic_notify"
+	'es_extended',
+	'mythic_notify'
 }
