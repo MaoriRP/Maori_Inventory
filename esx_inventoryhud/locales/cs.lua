06bc1ac1fa39b3241e52f336f2a8fc055fe28bb9
rex2630 <rex2630@gmail.com>
2020-03-28 21:32:11 +0100

    Updated czech locale

--- esx_inventoryhud/locales/cs.lua
+++ esx_inventoryhud/locales/cs.lua
@@ -1,6 +1,15 @@
 Locales['cs'] = {
-	['cash'] = 'Hotovost',
-	['black_money'] = 'Špinavé peníze',
-	['player_nearby'] = 'Zvolený hráč již není u tebe!',
-	['players_nearby'] = 'Poblíž tebe není žádný hrác!'
+	['cash'] = 'hotovost',
+	['black_money'] = 'špinavé peníze',
+	['player_nearby'] = 'zvolený hráč již není u tebe!',
+	['players_nearby'] = 'poblíž tebe není žádný hrác!',
+	['player_info'] = '<strong>Name:</strong> %s<br><strong>Váha:</strong> %s / %s KG.',
+	["player_info_full"] = "<h4 style='color:red;'>Inventář je plný</h4><br><strong>Name:</strong> %s<br><strong>Váha:</strong> %s / %s KG.",
+	['insufficient_space'] = 'inventář je plný!',
+	['player_inv_no_space'] = 'nemáš dostatek místa pro tento předmět ve tvém inventáři!',
+	['openinv_help'] = 'otevře inventář jiného hráče',
+	['openinv_id'] = 'hráčovo ID',
+	['no_permissions'] = 'k tomuto nemáš dostatečná oprávnění!',
+	['no_player'] = 'nebyl nalezen hráč s tímto ID!',
+	['player_inventory'] = 'inventář hráče'
 }
